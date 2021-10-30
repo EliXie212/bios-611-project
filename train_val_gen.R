94e6571807a8f4502a270c17fec21266fabff160
EliXie212 <zdxie@berkeley.edu>
2021-10-30 00:34:33 -0400

    Added file that does train/val split

---
+++ train_val_gen.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+
+set.seed(123)
+dat <- read.csv("source_data/heart.csv", sep = ",")
+
+
+## Split data into train_val and test
+split_dummy.1 <- sample(c(rep(0, 9/10 * nrow(dat)),
+                          rep(1, 1/10 * nrow(dat))))
+
+
+train.val.dat <- dat[split_dummy.1 == 0, ]
+test_dat <- dat[split_dummy.1 == 1, ]
+
+
+## Split train_val into train and validatioin
+split_dummy.2 <- sample(c(rep(0, 8/9 * nrow(train.val.dat)),
+                          rep(1, 1/9 * nrow(train.val.dat))))
+
+
+train_dat <- train.val.dat[split_dummy.2 == 0, ]
+val_dat <- train.val.dat[split_dummy.2 == 1, ]
+
+
+write(train_dat, 'derived_data/train_dat.csv')
+write(val_dat, 'derived_data/val_dat.csv')
