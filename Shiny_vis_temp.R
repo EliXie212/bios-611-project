b468eeeeee3fe4a1d22ae3aa84d74f11ad9fe082
EliXie212 <zdxie@berkeley.edu>
2021-11-02 15:20:32 -0400

    Modified shiny to solve port access issues

--- Shiny_vis_temp.R
+++ Shiny_vis_temp.R
@@ -7,19 +7,19 @@ metric_cont <- c("age", "chol", 'trestbps', 'oldpeak')
 
 
 ui <- shinyUI(fluidPage(
-  
+
   titlePanel("Heart Disease Visualization"),
   plotOutput('distPlot'),
   hr(),
   fluidRow(
-    
+
     ## Metric Selection
     column(4,
              sidebarPanel(width = 12,
                selectInput('metric', 'Continuous Metric', metric_cont, selected = metric_cont[[2]])
               ),
     ),
-    
+
     ## Bin Number Control
     column(8,
              sidebarPanel(width = 12,
@@ -41,22 +41,22 @@ server <- function(input, output) {
         # generate bins based on input$bins from ui.R
         x_1 <- heart[heart$target == 1, ][, input$metric]
         x_2 <- heart[heart$target == 0, ][, input$metric]
-        
-        
+
+
         min_v = min(min(x_1), min(x_2))
         max_v = max(max(x_1), max(x_2))
-        
+
         bins <- seq(min_v, max_v, length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
         hg_1 = hist(x_1, breaks = bins, plot=FALSE)
         hg_2 = hist(x_2, breaks = bins, plot=FALSE)
-        
+
         ## Specify colors
         c1 <- rgb(173,216,230,max = 255, alpha = 80, names = "lt.blue")
         c2 <- rgb(255,192,100, max = 255, alpha = 80, names = "lt.pink")
-        
-        
+
+
         plot(hg_1, col=c1, main='Heart Disease Visualization', xlab='chol')
         plot(hg_2, col=c2, add=TRUE)
         legend("topright",   # Position
@@ -66,5 +66,5 @@ server <- function(input, output) {
     })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run the application
+shinyApp(ui = ui, server = server, options=list(host='0.0.0.0', port=8722) )
