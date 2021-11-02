b868f3fc0976c8da20bd45d3bd66d378af76d0c4
EliXie212 <zdxie@berkeley.edu>
2021-11-02 15:22:11 -0400

    Modified shell file to solve file access issues on rstudio

--- start-env.sh
+++ start-env.sh
@@ -3,59 +3,11 @@ mkdir source_data
 mkdir figures
 mkdir derived_data
 
+sudo docker build -t project-env .
 
-function box_out()
-{
-  local s=("$@") b w
-  for l in "${s[@]}"; do
-    ((w<${#l})) && { b="$l"; w="${#l}"; }
-  done
-  tput setaf 3
-  echo " -${b//?/-}-
-| ${b//?/ } |"
-  for l in "${s[@]}"; do
-    printf '| %s%*s%s |\n' "$(tput setaf 4)" "-$w" "$l" "$(tput setaf 3)"
-  done
-  echo "| ${b//?/ } |
- -${b//?/-}-"
-  tput sgr 0
-}
 
-docker build . -t project_env
-
-MODE=$1
-
-if [ -z "$MODE" ] || [ "$MODE" == "rstudio" ];
-then
-export R_PASS=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo)
-docker run --rm \
- -p 8787:8787 \
- -p 8722:22 \
- -e PASSWORD=$R_PASS\
- -v $(pwd):/home/rstudio/bios611\
- -d\
- -t project_env > /tmp/docker-output
-
-export DOUT=$(cat /tmp/docker-output)
-
-echo $DOUT
-
-export CID=$(echo $DOUT | tail -1)
-
-box_out 'Log into RStudio on port 8787'\
-        'Username: rstudio'\
-        "Password: $R_PASS"\
-        'to kill the docker container'\
-        "run \"docker kill $CID\""
-
-fi
-
-if [ "$MODE" == "term" ];
-then
-x11docker --clipboard --share ~/.ssh --share $(pwd) --share ~/.emacs.d --share ~/.emacs-trash 611 /bin/xfce4-terminal
-fi
-
-if [ "$MODE" == "emacs" ];
-then
-x11docker --clipboard --share ~/.ssh --share $(pwd) --share ~/.emacs.d --share ~/.emacs-trash 611 /bin/emacs ~/work/courses/bios611/
-fi
+sudo docker run -v $(pwd):/home/rstudio \
+                  -p 8787:8787 \
+                  -p 8722:22 \
+                  -e PASSWORD=hello123 \
+                  -t project-env
