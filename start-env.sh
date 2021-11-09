67a36f97226efa224b1ae8fcb6de2f0b639b9626
EliXie212 <zdxie@berkeley.edu>
2021-11-09 18:47:16 -0500

    Modified bash file to resolve issues with packages failed to install

--- start-env.sh
+++ start-env.sh
@@ -3,10 +3,10 @@ mkdir source_data
 mkdir figures
 mkdir derived_data
 
-sudo docker build -t project-env .
+sudo docker build . -t project-env
 
 
-sudo docker run -v $(pwd):/home/rstudio \
+sudo docker run -v $(pwd):/home/rstudio/project \
                   -p 8787:8787 \
                   -p 8722:22 \
                   -e PASSWORD=hello123 \
