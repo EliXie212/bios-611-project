d28aa81f59114ae4db4231a165ecd515bde940f6
EliXie212 <zdxie@berkeley.edu>
2021-11-29 17:27:46 -0500

    Added additional port for dash app when initiatin docker container

--- start-env.sh
+++ start-env.sh
@@ -10,6 +10,7 @@ docker build . -t project-env
 
 docker run -v $(pwd):/home/rstudio/project \
                   -p 8787:8787 \
-                  -p 8722:22 \
+                  -p 8722:8722 \
+                  -p 8725:8725 \
                   -e PASSWORD=hello123 \
                   -t project-env
